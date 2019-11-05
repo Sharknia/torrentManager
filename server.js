7e00c41d86cf510c69d7f8369ac4f6ffd5aa6425
sharknia <furychick0@gmail.com>
2019-11-05 13:25:55 +0900

    sqlite3 test

--- server.js
+++ server.js
@@ -5,14 +5,32 @@ var session = require('express-session');
 var app = express();
 var sqlite3 = require('sqlite3').verbose();
 
-var db = new sqlite3.Database(':memory:', sqlite3.OPEN_READWRITE, (err) =>{
-    if(err){
-        console.log(err);
-    }
-    else{
-        console.log("db success");
-    }
+// var db = new sqlite3.Database(':memory:', sqlite3.OPEN_READWRITE, (err) =>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("db success");
+//     }
+// });
+
+var db = new sqlite3.Database(':memory:');
+
+db.serialize(function() {
+  db.run("CREATE TABLE lorem (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
 });
+
+db.close();
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
