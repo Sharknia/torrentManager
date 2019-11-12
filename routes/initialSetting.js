87acb53bd4003353217b70b8b45206f7b941f562
sharknia <furychick0@gmail.com>
2019-11-12 13:17:21 +0900

    즐겨찾기 디버깅

--- routes/initialSetting.js
+++ routes/initialSetting.js
@@ -25,7 +25,7 @@ router.post('/settingSave', function(req, res){
         db.run("CREATE TABLE 'dirPathList'('idx' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 'name' TEXT NOT NULL, 'path' TEXT NOT NULL)");
         //경로 설정 테이블에 insert /홈은 필수 지정(main - exlporer에서 보여줄 기본 폴더로 사용), watch 폴더는 torrent 파일 업로드를 위해 이름이 필수로 필요
         var intoDirPath = db.prepare('INSERT INTO dirPathList(name, path) VALUES(?, ?)');
-        intoDirPath.run('홈' , homeDir;
+        intoDirPath.run('홈' , homeDir);
         if(data.torrentDownloadDir != '') intoDirPath.run('다운로드' , data.torrentDownloadDir);
         if(data.tvProgramDir != '') intoDirPath.run('TV 폴더' , data.tvProgramDir);
         if(data.movieDir != '') intoDirPath.run('영화 폴더' , data.movieDir);
