f56a7122e6f2d0ad3a5fe9be001fca9b53045ae1
sharknia <furychick0@gmail.com>
2020-01-02 13:08:32 +0900

    디버깅

--- routes/main.js
+++ routes/main.js
@@ -112,12 +112,12 @@ router.post('/torrentSearch', function (req, res) {
     var count = '';
 
     client.fetch(url, param, function (err, $, response, body) {
-        // console.log("url : " + url);
-        // console.log("param : " + param);
-        // console.log("err : " + err);
-        // console.log("$ : " + $);
-        // console.log("response : " + response);
-        // console.log("body : " + body);
+        console.log("url : " + url);
+        console.log("param : " + param);
+        console.log("err : " + err);
+        console.log("$ : " + $);
+        console.log("response : " + response);
+        console.log("body : " + body);
         if (err) {
             console.log(err);
         }
